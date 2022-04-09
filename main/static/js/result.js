c08469925fd32ca42190b94e9b686e42dda65605
YunA486 <w2039@e-mirim.hs.kr>
2022-04-09 13:21:41 +0900

    Commit kakao_share

--- main/static/js/result.js
+++ main/static/js/result.js
@@ -12,7 +12,46 @@ function clip() {
 }
 
 function sharing() {
-	window.open('http://www.facebook.com/sharer/sharer.php?u=https://www.naver.com/');
+	window.open('http://www.facebook.com/sharer/sharer.php?u=https://vbti.herokuapp.com/result/<int:vegetable_id>');
 	// 서버 만들어서 u= 다음부터 넣기
 }
 
+import Kakao from '@/common/plugins/kakao.min'
+
+// SDK init
+if (!Kakao.isInitialized()) {
+	Kakao.init('38b66d063cf7a54f727a919e785ee441')
+}
+
+// createDefaultButton 함수 호출
+Kakao.Link.createDefaultButton({
+	container: '.kakao_share',
+	objectType: 'commerce',
+	content: {
+		   title: param.title,
+		   imageUrl,
+		   imageWidth,
+		   imageHeight,
+		   link: {
+			 mobileWebUrl: param.shareUrl,
+			 webUrl: param.shareUrl
+		   }
+	 },
+	 commerce: {
+		   regularPrice: param.regularPrice,
+		   discountPrice: param.discountPrice,
+		   discountRate: param.discountRate
+	 },
+	 buttons: [
+		   {
+			 title: '구매하기',
+			 link: {
+			   mobileWebUrl: param.shareUrl,
+			   webUrl: param.shareUrl
+			 }
+		   }
+	 ]
+})
+
+// const kakaoShare = document.querySelector('.kakao_share');
+// kakaoShare.init('38b66d063cf7a54f727a919e785ee441');
